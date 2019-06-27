66df932b3d662c2394558eca7971c5f4b9ef247c
Ubuntu <ubuntu@pool-342-08.informatik.uni-ulm.de>
2019-06-27 14:40:45 +0000

    configure.sh updated

--- configure.sh
+++ configure.sh
@@ -8,20 +8,20 @@ cd ~/tmpWorDir
 #wget -qO - https://packages.confluent.io/deb/5.2/archive.key | sudo apt-key add -
 #sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/5.2 stable main"
 #sudo apt-get update && sudo apt-get install -y librdkafka-dev
-git clone git@github.com:edenhill/librdkafka.git
+git clone https://github.com/edenhill/librdkafka.git
 cd librdkafka
-./configure
+./configure --install-deps
 make
 sudo make install
 cd ..
 
 # Installing Required SSL Package
-sudo apt install -y libssl-dev zlib1g-dev pkg-config
+#sudo apt install -y libssl-dev zlib1g-dev pkg-config
 
 # Installing Google Protobuf for C/C++
 sudo apt-get -y install autoconf automake libtool curl make g++ unzip
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protobuf-cpp-3.7.1.zip -P .
-unzip protobuf-cpp-3.7.1.zip -d .
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protobuf-cpp-3.7.1.zip -P ~/tmpWorDir
+unzip protobuf-cpp-3.7.1.zip -d ~/tmpWorDir
 cd protobuf-3.7.1
 ./configure
 make
